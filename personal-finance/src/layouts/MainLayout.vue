5b3fa78fb36a382d8cf7192b63cb6287bad826c6
Alex Muniz <munizalex45@gmail.com>
2023-08-11 17:43:12 +0000

    setup

--- personal-finance/src/layouts/MainLayout.vue
+++ personal-finance/src/layouts/MainLayout.vue
@@ -1,3 +1,4 @@
+/* eslint-disable */
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
@@ -6,14 +7,21 @@
         <q-toolbar-title>
           Personal Finance App
         </q-toolbar-title>
+        <q-btn align="right" class="btn-fixed-width" color="secondary" label="Login" />
 
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer elevated>
+      <q-tabs v-model="tab" class="text-Light">
+        <q-tab name="mails" icon="mail" label="Mails" />
+        <q-tab name="alarms" icon="alarm" label="Alarms" />
+        <q-tab name="movies" icon="movie" label="Movies" />
+      </q-tabs>
+    </q-footer>
   </q-layout>
 </template>
 
