5dd9a73d388067db4c00f25c8a3132b3953411f0
Alex Muniz <munizalex45@gmail.com>
2023-08-15 23:07:49 -0300

    frontend

--- personal-finance/src/pages/PageCategories.vue
+++ personal-finance/src/pages/PageCategories.vue
@@ -1,41 +1,39 @@
-/* eslint-disable */
 <template>
-    <scroll-page
-      title="Categories"
-      actionName="Add new category"
-      v-model:actionModel="showAddCategory"
-    >
-      <category-list :categories="categories" v-if="categories.length" />
-  
-      <no-resource-banner v-else>
-        There is no category yet. Add a first category to sort your expenses.
-      </no-resource-banner>
-  
-      <app-dialog v-model:showDialog="showAddCategory">
-        <add-category @close="showAddCategory = false" />
-      </app-dialog>
-    </scroll-page>
-  </template>
-  
-  <script>
-  import { mapGetters } from 'vuex';
-  import mixinPage from 'src/mixins/mixin-page';
-  import CategoryList from 'src/components/Categories/List/CategoryList';
-  import AddCategory from 'src/components/Categories/Modals/AddCategory';
-  
-  export default {
-    mixins: [mixinPage],
-    data() {
-      return {
-        showAddCategory: false,
-      };
-    },
-    components: {
-      CategoryList,
-      AddCategory,
-    },
-    computed: {
-      ...mapGetters('categories', ['categories']),
-    },
-  };
-  </script>
+  <scroll-page
+  title="Categories"
+  actionName="Add new category"
+  v-model:actionModel="showAddCategory">
+    <category-list :categories="categories" v-if="categories.length" />
+
+    <no-resource-banner v-else>
+      There is no category yet. Add a first category to sort your expenses.
+    </no-resource-banner>
+
+    <app-dialog v-model:showDialog="showAddCategory">
+      <add-category @close="showAddCategory = false" />
+    </app-dialog>
+  </scroll-page>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import mixinPage from 'src/mixins/mixin-page';
+import CategoryList from 'src/components/Categories/List/CategoryList';
+import AddCategory from 'src/components/Categories/Modals/AddCategory';
+
+export default {
+  mixins: [mixinPage],
+  data() {
+    return {
+      showAddCategory: false,
+    };
+  },
+  components: {
+    CategoryList,
+    AddCategory,
+  },
+  computed: {
+    ...mapGetters('categories', ['categories']),
+  },
+};
+</script>
--- personal-finance/src/stores/app/getters.js
+++ personal-finance/src/stores/app/getters.js
@@ -1,7 +1,7 @@
 export function appReady(state, getters, rootState, rootGetters) {
-    return !!rootGetters['users/currentUser'] && rootGetters['categories/categories'];
-  }
-  
-  export function projectsLoaded(state) {
-    return state.projectsLoaded;
-  }
+  return !!rootGetters['users/currentUser'] && rootGetters['categories/categories'];
+}
+
+export function projectsLoaded(state) {
+  return state.projectsLoaded;
+}
