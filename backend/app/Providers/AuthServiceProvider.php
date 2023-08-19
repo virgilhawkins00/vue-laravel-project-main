119644d133f54e9bc169f1c366572f9ba15d192e
Bruno S <bruno1pb13@gmail.com>
2023-08-18 21:30:40 -0300

    Revert "Integracao autenticacao"

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -1,18 +1,26 @@
 <?php
 
 namespace App\Providers;
+
+// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
-use App\Firebase\Guard as FirebaseGuard;
-use App\Firebase\FirebaseUserProvider;
-class AuthServiceProvider extends ServiceProvider {
-   protected $policies = [
-      // 'App\Model' => 'App\Policies\ModelPolicy',
-   ];
-   public function boot() {
-      $this->registerPolicies();
-      \Illuminate\Support\Facades\Auth::provider('firebaseuserprovider', function($app, array $config) {
-         return new FirebaseUserProvider($app['hash'], $config['model']);
-      });
-   }
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The model to policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        //
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     */
+    public function boot(): void
+    {
+        //
+    }
 }
