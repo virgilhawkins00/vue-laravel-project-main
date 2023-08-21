9cb4c257b33fe3161ce30ec29c31d19f41710e7b
Bruno S <bruno1pb13@gmail.com>
2023-08-21 17:48:54 +0000

    i don't know

--- personal-finance/src/boot/router-auth.js
+++ personal-finance/src/boot/router-auth.js
@@ -1,6 +1,7 @@
 import { LocalStorage } from 'quasar';
 
 export default async ({ router, store }) => {
+<<<<<<< HEAD
   const user = LocalStorage.getItem('user');
   if (user !== null) {
     store.dispatch('app/loggedIn', user, { root: true });
@@ -18,3 +19,25 @@ export default async ({ router, store }) => {
   //   }
   });
 };
+=======
+    const user = await LocalStorage.getItem('user');
+    if (user !== null) {
+      store.dispatch('app/loggedIn', user, { root: true });
+    }
+
+
+  
+    router.beforeEach((to, from, next) => {
+      // const currentUser = store.getters['users/currentUser'];
+
+      // if (!currentUser && to.path !== '/login') {
+      //   next('/login');
+      // } else if (currentUser && to.path === '/login') {
+      //   next('/');
+      // } else {
+        next();
+      // }
+    })
+  
+}
+>>>>>>> 94e056a (i don't know)
